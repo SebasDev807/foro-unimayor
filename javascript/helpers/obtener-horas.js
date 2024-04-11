30988c0f582d6a721eb6a5b968f6753e03eb320e
Sebastián <sebastian.dev0708@gmail.com>
2024-04-11 10:09:25 -0500

    Configurando el formato d ela hora(AM/PM)

--- javascript/helpers/obtener-horas.js
+++ javascript/helpers/obtener-horas.js
@@ -1,9 +1,19 @@
 function obtenerHora() {
-    let now = new Date();
-    let horas = now.getHours();
-    let minutos = now.getMinutes();
-    let horaFormateada = `${horas} : ${minutos < 10 ? '0' + minutos : minutos}`;
-    return horaFormateada;
+  let now = new Date();
+  let horas = now.getHours();
+  let minutos = now.getMinutes();
+  let ampm = horas >= 12 ? "PM" : "AM";
+
+  if (horas > 12) {
+    horas -= 12;
+  } else if (horas === 0) {
+    horas = 12;
+  }
+
+  let horaFormateada = `${horas}:${
+    minutos < 10 ? "0" + minutos : minutos
+  } ${ampm}`;
+  return horaFormateada;
 }
 
 export { obtenerHora };
