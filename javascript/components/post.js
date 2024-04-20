8df429b0f0104fa6977badaa4b0ad789fbfd1b63
Sebastián <sebastian.dev0708@gmail.com>
2024-04-20 15:08:01 -0500

    ..

--- javascript/components/post.js
+++ javascript/components/post.js
@@ -1,84 +1,68 @@
 import { obtenerFecha, obtenerHora } from "../helpers/obtener-tiempo.js";
 
-const PostComponent = (imgUrl, userName, categoryHashTagt) => {
-  const listaPreguntas = document.querySelector(".list-posts");
-//   const botonPublicar = document.querySelector(".boton");
-  const inputTitulo = document.querySelector(".titulo-pregunta");
-  const inputCuerpo = document.querySelector(".cuerpo-pregunta");
+const contenedorPregunta = document.querySelector("#pregunta");
+const ulPosts = document.querySelector("#lista-posts");
+let listaPost = [];
 
-  const elemento = document.createElement("li");
-  const articulo = document.createElement("article");
-  const tituloPregunta = document.createElement("h2");
-  const textoCuerpo = document.querySelector("p");
+cargarEventListeners();
 
-  const contenedorUsuario = document.createElement("div");
-  const fotoPerfil = document.createElement("img");
-  const nombreUsuario = document.createElement("p");
-  const hashTag = document.createElement("p");
-  const horas = document.createElement("p");
-  const fecha = document.createElement('p');
+function cargarEventListeners() {
+  contenedorPregunta.addEventListener("click", postearPregunta);
+}
 
-  const contenedorBotones = document.createElement('div');
-  const botonSubir = document.createElement('button');
-  const botonBajar = document.createElement('button');
-  const botonComentar = document.createElement('button');
-  
-  const categoria = document.createElement('p');
-  
-  //configuracion de los elementos
-  fotoPerfil.src = imgUrl // "/imagenes/leon-foto-perfil.png";
-  nombreUsuario.textContent = userName;
-  hashTag.textContent = categoryHashTagt;
+function postearPregunta(e) {
+  if (e.target.classList.contains("boton")) {
+    const post = e.target.parentElement.previousElementSibling.children[1];
+    leerDatos(post);
 
-  horas.textContent = obtenerHora();
-  fecha.textContent = obtenerFecha();
+    // Reiniciar los campos de entrada a su estado original (vacío)
+    post.querySelector("input").value = "";
+    post.querySelector("textarea").value = "";
+    post.querySelector("textarea").style.height = "";
+  }
+}
 
+function leerDatos(post) {
+  const infoPost = {
+    imagen: "",
+    titulo: post.querySelector("input").value,
+    cuerpo: post.querySelector("textarea").value,
+    hora: obtenerHora(),
+    fecha: obtenerFecha(),
+  };
+  listaPost = [...listaPost, infoPost];
+  console.log(listaPost);
+  postPreguntaHTML();
+}
 
+function postPreguntaHTML() {
+  limpiarLista();
+  listaPost.forEach((post) => {
+    const { titulo, cuerpo, fecha, hora } = post;
+    const contenedorPost = document.createElement("article");
+    contenedorPost.classList.add("contenedor-post");
+    contenedorPost.innerHTML = `
+        <div class="contenedor-usuario">
+            <p>NekitoKawaii87</p>
+            <p>SempaiMaster</p>
+            <p>${fecha}</p>        
+        </div>
+        <h2>${titulo}</h2>
+        <p>${cuerpo}</p>
+        <div class="contenedor-botones-post">
+            <button class="btn-subir">sube</button>
+            <button class="btn-bajar">baja</button>
+            <button class="btn-comentar">comenta</button>
+            <p>Emprendimiento</p>
+            <p>${hora}</p>
+        </div>
+    `;
+    ulPosts.insertBefore(contenedorPost, ulPosts.firstElementChild);
+  });
+}
 
-  botonSubir.textContent = "sube";
-  botonBajar.textContent = "baja";
-  botonComentar.textContent = "comenta";
-  categoria.textContent = "Emprendimiento";
-
-  //Asignar clases
-  contenedorBotones.classList.add("contenedor-botones-post");
-  botonSubir.classList.add("btn-subir");
-  botonBajar.classList.add("btn-bajar");
-  botonComentar.classList.add("btn-comentar");
-  contenedorUsuario.classList.add("contenedor-usuario");
-  elemento.classList.add("pregunta-post");
-  articulo.classList.add("contenedor-post");
-
-  tituloPregunta.textContent = inputTitulo.value;
-  textoCuerpo.textContent = inputCuerpo.value;
-
-  //Agregar al DOM
-  contenedorUsuario.appendChild(fotoPerfil);
-  contenedorUsuario.appendChild(nombreUsuario);
-  contenedorUsuario.appendChild(hashTag);
-  contenedorUsuario.appendChild(fecha);
-
-
-  contenedorBotones.appendChild(botonSubir);
-  contenedorBotones.appendChild(botonBajar);
-  contenedorBotones.appendChild(botonComentar);
-  contenedorBotones.appendChild(categoria);
-  contenedorBotones.appendChild(horas);
-
-  articulo.appendChild(contenedorUsuario);
-  articulo.appendChild(tituloPregunta);
-  articulo.appendChild(textoCuerpo);
-  articulo.appendChild(contenedorBotones);
-
-  articulo.appendChild(contenedorUsuario);
-  articulo.appendChild(tituloPregunta);
-  articulo.appendChild(textoCuerpo);
-  articulo.appendChild(contenedorBotones);
-
-  elemento.appendChild(articulo);
-
-  console.log(elemento);
-  return elemento;
-};
-
-export { PostComponent };
+function limpiarLista() {
+  while (ulPosts.firstChild) {
+    ulPosts.removeChild(ulPosts.firstChild);
+  }
+}
