a6bfcb90bf0a9013a09fc06a55c27ee5b59cf1ef
Sebastián <sebastian.dev0708@gmail.com>
2024-04-28 17:18:41 -0500

    renovando los estilos

---
+++ src/javascript/modal-comentarios.js
@@ -0,0 +1,22 @@
+// export function comentariosHTML() {
+//   const contenedor = document.querySelector("#seccion-preguntas");
+//   const comentarios = document.createElement("dialog");
+//   comentarios.id = "caja-comentarios";
+//   comentarios.innerHTML = `
+//           <div class="header-comentarios">
+//             <h3>Titulo</h3>
+//             <button id="btn-cerrar-comentarios">X</button>
+//           </div>
+//           <div class="contenedor-comentarios">
+//             <ul class="lista-respuestas">
+//               <h1>Aun no hay comentarios</h1>
+//             </ul>
+//           </div>
+//           <div class="footer-comentarios">
+//             <input type="text" placeholder="Agrega un comentario">
+//             <button>avión</button>
+//           </div>
+//   `;
+//   contenedor.appendChild(comentarios);
+
+// }
