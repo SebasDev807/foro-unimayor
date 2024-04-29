99eea0edeb89f9bcce8382c873161c4e9900bde0
Sebastián <sebastian.dev0708@gmail.com>
2024-04-28 20:55:54 -0500

    Agregando modal

--- src/javascript/modal-comentarios.js
+++ src/javascript/modal-comentarios.js
@@ -1,22 +1,33 @@
-// export function comentariosHTML() {
-//   const contenedor = document.querySelector("#seccion-preguntas");
-//   const comentarios = document.createElement("dialog");
-//   comentarios.id = "caja-comentarios";
-//   comentarios.innerHTML = `
-//           <div class="header-comentarios">
-//             <h3>Titulo</h3>
-//             <button id="btn-cerrar-comentarios">X</button>
-//           </div>
-//           <div class="contenedor-comentarios">
-//             <ul class="lista-respuestas">
-//               <h1>Aun no hay comentarios</h1>
-//             </ul>
-//           </div>
-//           <div class="footer-comentarios">
-//             <input type="text" placeholder="Agrega un comentario">
-//             <button>avión</button>
-//           </div>
-//   `;
-//   contenedor.appendChild(comentarios);
+const contenedor = document.querySelector("#seccion-preguntas");
+const comentarios = document.createElement("dialog");
 
-// }
+export function comentariosHTML() {
+  comentarios.id = "caja-comentarios";
+  comentarios.innerHTML = `
+    <div class="header-comentarios">
+      <h3>Titulo</h3>
+      <button id="btnCerrarModal">X</button>
+    </div>
+    <div class="contenedor-comentarios">
+      <ul class="lista-respuestas">
+        <h1>Aun no hay comentarios</h1>
+      </ul>
+    </div>
+    <div class="footer-comentarios">
+      <input type="text" placeholder="Agrega un comentario">
+      <button>avión</button>
+    </div>
+  `;
+  contenedor.appendChild(comentarios);
+  comentarios.classList.add("caja-comentarios");
+  comentarios.showModal();
+
+  const btnCerrarModal = comentarios.querySelector("#btnCerrarModal");
+
+  btnCerrarModal.addEventListener("click", () => {
+    comentarios.close();
+    comentarios.classList.remove("caja-comentarios");
+  });
+}
+
+
